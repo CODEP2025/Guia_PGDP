8511dfcb94ed9f241f07d8398e24550584d0c151
CODEP2025 <instrutor2@uneb.br>
2025-09-25 09:47:13 -0300

    urls.py

--- guia/urls.py
+++ guia/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, preview_procedure
 
 urlpatterns = [
     path('', home, name='home'),
+    path('preview/', preview_procedure, name='preview_procedure'),
 ]
